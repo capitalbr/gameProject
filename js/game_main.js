49dbae45ab42afc0fc2dd1774aaf8a356d6f0e60
Bradley <bbarnes.me@gmail.com>
2019-08-10 17:15:18 -0500

    Add shooting functionality

--- js/game_main.js
+++ js/game_main.js
@@ -1,6 +1,7 @@
 
 
 import * as THREE from 'three';
+import * as CANNON from 'cannon';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
@@ -14,6 +15,7 @@ let color = new THREE.Color();
 let direction = new THREE.Vector3();
 let vertex = new THREE.Vector3();
 let velocity = new THREE.Vector3();
+let velocity2 = new THREE.Vector3();
 let camera, scene, renderer, controls, raycaster;
 
 //TESTING
@@ -40,7 +42,115 @@ let forward = false;
 let back = false;
 let left = false;
 let right = false;
+let strafeL = false;
+let strafeR = false;
 let jumpAllow = false;
+
+//SHOOTING
+let world;
+let physicsMaterial;
+let sphereShape;
+let sphereBody;
+let balls = [];
+let material2;
+let ballMeshes = [], boxes = [], boxMeshes = [];
+let dt = 1 / 60;
+let ballShape = new CANNON.Sphere(0.2);
+let ballGeometry = new THREE.SphereGeometry(ballShape.radius, 32, 32);
+let shootDirection = new THREE.Vector3();
+let shootVelo = 950;
+  // var projector = new THREE.Projector();
+
+
+
+function initCannon(mainCharacter) {
+  // Setup our world
+  world = new CANNON.World();
+  world.quatNormalizeSkip = 0;
+  world.quatNormalizeFast = false;
+
+  var solver = new CANNON.GSSolver();
+
+  world.defaultContactMaterial.contactEquationStiffness = 1e9;
+  world.defaultContactMaterial.contactEquationRelaxation = 4;
+
+  solver.iterations = 7;
+  solver.tolerance = 0.1;
+  var split = true;
+  if (split)
+    world.solver = new CANNON.SplitSolver(solver);
+  else
+    world.solver = solver;
+
+  world.gravity.set(0, -20, 0);
+  world.broadphase = new CANNON.NaiveBroadphase();
+
+  // Create a slippery material (friction coefficient = 0.0)
+  physicsMaterial = new CANNON.Material("slipperyMaterial");
+  var physicsContactMaterial = new CANNON.ContactMaterial(physicsMaterial,
+    physicsMaterial,
+    0.0, // friction coefficient
+    0.3  // restitution
+  );
+  // We must add the contact materials to the world
+  world.addContactMaterial(physicsContactMaterial);
+
+  // Create a sphere (PHYSICS FOR THE MAINCHARACTER WHO IS JUST AN INVISIBLE SPHERE)
+  var mass = 5, radius = 1.3;
+  sphereShape = new CANNON.Sphere(radius);
+  sphereBody = new CANNON.Body({ mass: mass });
+  sphereBody.addShape(sphereShape);
+  // sphereBody.position.set(0, 5, 0);
+  sphereBody.position.set(mainCharacter.scene.position.x, mainCharacter.scene.position.y, mainCharacter.scene.position.z);
+  sphereBody.linearDamping = 0.9;
+  world.add(sphereBody);
+}
+
+ 
+
+function getShootDir(targetVec) {
+  var vector = targetVec;
+  targetVec.set(0, 0, 1);
+  vector.unproject(camera);
+  var ray = new THREE.Ray(sphereBody.position, vector.sub(sphereBody.position).normalize());
+  targetVec.copy(ray.direction);
+}
+
+window.addEventListener("click", function (e) {
+  // if (controls.enabled == true) {
+    var x = sphereBody.position.x;
+    var y = sphereBody.position.y;
+    var z = sphereBody.position.z;
+    console.log(`xyx ${x}, ${y}, ${z}`)
+    this.console.log(`mainCharacter ${mainCharacter.scene.position.x}, ${mainCharacter.scene.position.y}, ${mainCharacter.scene.position.z}`)
+    var ballBody = new CANNON.Body({ mass: 0.00000000000000001 });
+    ballBody.addShape(ballShape);
+    var randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+    material2 = new THREE.MeshPhongMaterial({ color: randomColor });
+    var ballMesh = new THREE.Mesh(ballGeometry, material2);
+    world.add(ballBody);
+    scene.add(ballMesh);
+    ballMesh.castShadow = true;
+    ballMesh.receiveShadow = true;
+    balls.push(ballBody);
+    ballMeshes.push(ballMesh);
+    getShootDir(shootDirection);
+    this.console.log(shootDirection);
+    ballBody.velocity.set(shootDirection.x * shootVelo,
+      shootDirection.y * shootVelo,
+      shootDirection.z * shootVelo);
+
+    // Move the ball outside the player sphere
+    x += shootDirection.x * (sphereShape.radius * 1.02 + ballShape.radius);
+    y += shootDirection.y * (sphereShape.radius * 1.02 + ballShape.radius);
+    z += shootDirection.z * (sphereShape.radius * 1.02 + ballShape.radius);
+    ballBody.position.set(x, y, z);
+    ballMesh.position.set(x, y, z);
+  // }
+});
+
+//END SHOOTING
+
 
 
 const deleteObAddOb = (name, ObToAdd) => {
@@ -59,7 +169,7 @@ const characterCreator = () => {
   let loader = new GLTFLoader().setPath('https://assets-yp9dzdxebv.s3.us-east-2.amazonaws.com/');
   loader.load('mainCharWorkingReadyForExportWithNLH.glb', function (gltf) {
     
-    
+   
     mainCharacter = gltf;
     mainCharacter.scene.scale.set(3, 3, 3);
     mainCharacter.aHash = {};
@@ -119,6 +229,7 @@ const characterCreator = () => {
     mainCharacter.scene.position.set(0, 0, 0);
     mainCharacter.scene.add(cameraHolder);
     scene.add(mainCharacter.scene);
+    initCannon(mainCharacter);
 
   }, undefined, function (error) {
 
@@ -322,6 +433,9 @@ const characterCreator = () => {
   mirrorSphereCamera.position.set(75, 50, 0);
   scene.add(mirrorSphere);
 
+
+
+  
 }
 
 
@@ -374,19 +488,19 @@ function init() {
 
   // });
 
-  // controls.addEventListener('lock', function () {
+  // // controls.addEventListener('lock', function () {
 
-  //   instructions.style.display = 'none';
-  //   game.style.display = 'none';
+  // //   instructions.style.display = 'none';
+  // //   game.style.display = 'none';
 
-  // });
+  // // });
 
-  // controls.addEventListener('unlock', function () {
+  // // controls.addEventListener('unlock', function () {
 
-  //   game.style.display = 'block';
-  //   instructions.style.display = '';
+  // //   game.style.display = 'block';
+  // //   instructions.style.display = '';
 
-  // });
+  // // });
 
   // scene.add(controls.getObject());
   // END POINTERLOCK
@@ -414,7 +528,14 @@ function init() {
         right = true;
         
         break;
+      case 37:
+        strafeL = true;
 
+        break;
+      case  39:
+        strafeR = true;
+
+        break;
       case 32: // space
         if (jumpAllow === true) velocity.y += 350;
         jumpAllow = false;
@@ -447,7 +568,14 @@ function init() {
         right = false;
         
         break;
+      case 37:
+        strafeL = false;
 
+        break;
+      case 39:
+        strafeR = false;
+
+        break;
     }
 
   };
@@ -493,6 +621,7 @@ function init() {
   let floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors });
 
   let floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  // floor.visible = false;
   scene.add(floor);
 
 
@@ -553,16 +682,19 @@ function animate() {
     let delta = (time - lastTime) / 1000;
 
     velocity.x -= velocity.x * 10.0 * delta;
+    velocity2.x -= velocity2.x * 10.0 * delta;
     velocity.z -= velocity.z * 10.0 * delta;
 
     velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
 
     direction.z = Number(forward) - Number(back);
     direction.x = Number(left) - Number(right);
+    direction.x2 = Number(strafeL) - Number(strafeR);
     direction.normalize(); // this ensures consistent movements in all directions
 
     if (forward || back) velocity.z -= direction.z * 400.0 * delta;
     if (left || right) velocity.x -= direction.x * 400.0 * delta;
+    if (strafeL || strafeR) velocity2.x -= direction.x2 * 400.0 * delta;
 
     // controls.getObject().translateX(velocity.x * delta);
     // controls.getObject().position.y += (velocity.y * delta);
@@ -572,15 +704,21 @@ function animate() {
     
     if (mainCharacter) {
       
-      mainCharacter.scene.rotation.y += ((velocity.x * delta)/ 10) * -1;
+      mainCharacter.scene.rotation.y += ((velocity.x * delta)/ 10);
       mainCharacter.scene.position.y += (velocity.y * delta);
       mainCharacter.scene.translateZ(velocity.z * delta * -1);
+      mainCharacter.scene.translateX(velocity2.x * delta * -1);
+      sphereBody.position.x = mainCharacter.scene.position.x;
+      sphereBody.position.y = mainCharacter.scene.position.y + 10;
+      sphereBody.position.z = mainCharacter.scene.position.z;
    
       let mType = "idle";
       if (forward) mType = {type: "forward"};
       if (left) mType = {type: "left"};
       if (right) mType = {type: "right"};
       if (back) mType = {type: "back"}; 
+      if (strafeL) mType = {type: "strafeL"};
+      if (strafeR) mType = {type: "strafeR"};
       switch (mType.type) {
         case "forward":
           playClip("Run");
@@ -592,6 +730,12 @@ function animate() {
           playClip("StrafeLeft");
           break;
         case "right":
+          playClip("Strafe2");
+          break;
+        case "strafeL":
+          playClip("StrafeLeft");
+          break;
+        case "strafeR":
           playClip("Strafe2");
           break;
         default:
@@ -647,6 +791,23 @@ function animate() {
   mirrorSphereCamera.updateCubeMap(renderer, scene);
   mirrorSphere.visible = true;
   // END REFLECTIVE OBJECTS
+
+
+  //CANNON
+  world.step(dt);
+
+  // Update ball positions
+  for (var i = 0; i < balls.length; i++) {
+    ballMeshes[i].position.copy(balls[i].position);
+    ballMeshes[i].quaternion.copy(balls[i].quaternion);
+  }
+  //     if (count < 0) {
+  //       debugger
+  //       count = 400;
+  //     }
+  // count -= 1
+  //END CANNON
+
 
 
   // renderer.render(scene, camera);
